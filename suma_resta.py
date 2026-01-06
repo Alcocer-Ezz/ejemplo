9bff29881d1c82e23ae2a01310e66d124c02b104
davozvm <dvalencia@ezzquad.com>
2026-01-06 12:57:10 -0600

    Se mejoró la version sin refactorizar añadiendo unos mensajes: de bienvenida, despedida, y mejor lectura a la hora de ejecutar el código

--- suma_resta.py
+++ suma_resta.py
@@ -1,11 +1,15 @@
-# Script sin refactorizar: pide dos números, los suma y los resta
+print("Bienvenido \n")
 try:
 	a = float(input('Ingrese el primer número: '))
 	b = float(input('Ingrese el segundo número: '))
+
 except ValueError:
 	print('Entrada inválida: por favor ingrese números.')
+
 else:
 	suma = a + b
 	resta = a - b
-	print('Suma:', suma)
-	print('Resta:', resta)
+	print('\nSuma:', suma)
+	print('\nResta:', resta)
+
+print("\nGracias por usar el programa")
